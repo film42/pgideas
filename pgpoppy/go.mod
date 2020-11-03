4bd8b5131d0c8045fc7759aea8c359bb2fa233d8
Garrett Thornburg <film42@gmail.com>
2020-11-02 23:28:40 -0800

    Pgbouncer operator: Can gen config from yaml -> pgbouncer ini

---
+++ pgpoppy/go.mod
@@ -0,0 +1,10 @@
+module github.com/film42/pgideas/pgpoppy
+
+go 1.15
+
+require (
+	github.com/kr/pretty v0.2.1
+	github.com/pelletier/go-toml v1.8.1
+	gopkg.in/ini.v1 v1.62.0
+	gopkg.in/yaml.v2 v2.3.0
+)
---
+++ pgpoppy/main.go
@@ -0,0 +1,191 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	"github.com/kr/pretty"
+	"gopkg.in/ini.v1"
+	"gopkg.in/yaml.v2"
+)
+
+// Pgbouncer
+
+type PgbouncerConfigFile struct {
+	Pgbouncer *PgbouncerOptionsConfig `ini:"pgbouncer"`
+	Databases map[string]string       `ini:"databases"`
+}
+
+func (c *PgbouncerConfigFile) Marshal() ([]byte, error) {
+	cfg := ini.Empty()
+
+	// Write pgbouncer options
+	err := cfg.Section("pgbouncer").ReflectFrom(c.Pgbouncer)
+	if err != nil {
+		return nil, err
+	}
+
+	// Write databases
+	for db, opts := range c.Databases {
+		cfg.Section("databases").Key(db).SetValue(fmt.Sprintf(`"%s"`, opts))
+	}
+
+	// Write to bytes
+	buf := &bytes.Buffer{}
+	_, err = cfg.WriteTo(buf)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+// Poppy
+type Config struct {
+	PoppyConfig     *PoppyConfig     `yaml:"pgpoppy"`
+	PgbouncerConfig *PgbouncerConfig `yaml:"pgbouncer"`
+}
+
+func (c *Config) ToPgbouncerConfigFile() (*PgbouncerConfigFile, error) {
+	pgbouncerOpts := c.PgbouncerConfig.Options
+
+	databases := map[string]string{}
+	for _, database := range c.PgbouncerConfig.Databases {
+		name := database.DatabaseName
+		options := database.GetOptions()
+		databases[name] = options
+	}
+
+	return &PgbouncerConfigFile{
+		Pgbouncer: pgbouncerOpts,
+		Databases: databases,
+	}, nil
+}
+
+type PoppyConfig struct {
+	PgbouncerConnectionOptions string `yaml:"pgbouncer_connection_options"`
+}
+
+type PgbouncerConfig struct {
+	Databases []*DatabaseConfig       `yaml:"databases"`
+	Options   *PgbouncerOptionsConfig `yaml:"options"`
+}
+
+type PgbouncerOptionsConfig struct {
+	AdminUsers  string `yaml:"admin_users" ini:"admin_users,omitempty"`
+	AuthFile    string `yaml:"auth_file" ini:"auth_file,omitempty"`
+	AuthHbaFile string `yaml:"auth_hba_file" ini:"auth_hba_file,omitempty"`
+	AuthType    string `yaml:"auth_type" ini:"auth_type,omitempty"`
+
+	ClientIdleTimeout  int64  `yaml:"client_idle_timeout" ini:"client_idle_timeout,omitempty"`
+	ClientTlsCaFile    string `yaml:"client_tls_ca_file" ini:"client_tls_ca_file,omitempty"`
+	ClientTlsCertFile  string `yaml:"client_tls_cert_file" ini:"client_tls_cert_file,omitempty"`
+	ClientTlsCiphers   string `yaml:"client_tls_ciphers" ini:"client_tls_ciphers,omitempty"`
+	ClientTlsKeyFile   string `yaml:"client_tls_key_file" ini:"client_tls_key_file,omitempty"`
+	ClientTlsProtocols string `yaml:"client_tls_protocols" ini:"client_tls_protocols,omitempty"`
+	ClientTlsSslmode   string `yaml:"client_tls_sslmode" ini:"client_tls_sslmode,omitempty"`
+
+	ServerConnectTimeout int64  `yaml:"server_connect_timeout" ini:"server_connect_timeout,omitempty"`
+	ServerIdleTimeout    int64  `yaml:"server_idle_timeout" ini:"server_idle_timeout,omitempty"`
+	ServerTlsCaFile      string `yaml:"server_tls_ca_file" ini:"server_tls_ca_file,omitempty"`
+	ServerTlsCertFile    string `yaml:"server_tls_cert_file" ini:"server_tls_cert_file,omitempty"`
+	ServerTlsCiphers     string `yaml:"server_tls_ciphers" ini:"server_tls_ciphers,omitempty"`
+	ServerTlsKeyFile     string `yaml:"server_tls_key_file" ini:"server_tls_key_file,omitempty"`
+	ServerTlsProtocols   string `yaml:"server_tls_protocols" ini:"server_tls_protocols,omitempty"`
+	ServerTlsSslmode     string `yaml:"server_tls_sslmode" ini:"server_tls_sslmode,omitempty"`
+	ServerCheckQuery     string `yaml:"server_check_query" ini:"server_check_query,omitempty"`
+
+	DefaultPoolSize         int64  `yaml:"default_pool_size" ini:"default_pool_size,omitempty"`
+	IgnoreStartupParameters string `yaml:"ignore_startup_parameters" ini:"ignore_startup_parameters,omitempty"`
+	ListenAddr              string `yaml:"listen_addr" ini:"listen_addr,omitempty"`
+	ListenPort              string `yaml:"listen_port" ini:"listen_port,omitempty"`
+	MaxClientConn           int64  `yaml:"max_client_conn" ini:"max_client_conn,omitempty"`
+	MinPoolSize             int64  `yaml:"min_pool_size" ini:"min_pool_size,omitempty"`
+	PoolMode                string `yaml:"pool_mode" ini:"pool_mode,omitempty"`
+	ReservePoolSize         int64  `yaml:"reserve_pool_size" ini:"reserve_pool_size,omitempty"`
+	ReservePoolTimeout      string `yaml:"reserve_pool_timeout" ini:"reserve_pool_timeout,omitempty"`
+	RestartOnChange         bool   `yaml:"restart_on_change" ini:"restart_on_change,omitempty"`
+}
+
+type DatabaseConfig struct {
+	DatabaseName    string                 `yaml:"database"`
+	DatabaseOptions *DatabaseOptionsConfig `yaml:"options"`
+	Servers         []*ServerConfig        `yaml:"servers"`
+	Healthcheck     *HealthcheckConfig     `yaml:"healthcheck"`
+}
+
+func (dc *DatabaseConfig) GetOptions() string {
+	options := []string{}
+	if dc.DatabaseOptions.PoolSize != 0 {
+		options = append(options, fmt.Sprintf("pool_size=%d", dc.DatabaseOptions.PoolSize))
+	}
+	if dc.DatabaseOptions.PoolMode != "" {
+		options = append(options, fmt.Sprintf("pool_mode=%s", dc.DatabaseOptions.PoolMode))
+	}
+	if dc.DatabaseOptions.Port != 0 {
+		options = append(options, fmt.Sprintf("port=%d", dc.DatabaseOptions.Port))
+	}
+	if dc.DatabaseOptions.ClientEncoding != "" {
+		options = append(options, fmt.Sprintf("client_encoding=%s", dc.DatabaseOptions.ClientEncoding))
+	}
+	if dc.DatabaseOptions.Dbname != "" {
+		options = append(options, fmt.Sprintf("dbname=%s", dc.DatabaseOptions.Dbname))
+	}
+
+	// Getting the host should be checked but for now it will be the first. It is expected to be there.
+	options = append(options, fmt.Sprintf("host=%s", dc.Servers[0].ServerHost))
+
+	return strings.Join(options, " ")
+}
+
+type DatabaseOptionsConfig struct {
+	PoolSize       int64  `yaml:"pool_size"`
+	PoolMode       string `yaml:"pool_mode"`
+	Port           int64  `yaml:"port"`
+	ClientEncoding string `yaml:"client_encoding"`
+	Dbname         string `yaml:"dbname"`
+}
+
+type ServerConfig struct {
+	ServerName string `yaml:"name"`
+	ServerHost string `yaml:"host"`
+}
+
+type HealthcheckConfig struct {
+	Port int64  `yaml:"port"`
+	URI  string `yaml:"uri"`
+}
+
+func ParseConfig(file string) (*Config, error) {
+	bytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg := new(Config)
+	err = yaml.Unmarshal(bytes, &cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
+
+func main() {
+	cfg, err := ParseConfig("./examples/config.yaml")
+	if err != nil {
+		panic(err)
+	}
+	pretty.Println(cfg)
+
+	pgbouncerCfgFile, err := cfg.ToPgbouncerConfigFile()
+	if err != nil {
+		panic(err)
+	}
+	cfgFile, err := pgbouncerCfgFile.Marshal()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(cfgFile))
+}
